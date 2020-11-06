ea573d55661eb4c19e32e755d81465da99efb38a
Debian <azureuser@dev2.txdlcteiflhu1pgzcrqoftiioa.cx.internal.cloudapp.net>
2020-11-06 23:11:21 +0000

    doc and azinit

--- azinit.sh
+++ azinit.sh
@@ -8,27 +8,32 @@ set -x
 # kubectl get nodes ]??
 
 # 1. create cluster
-# az aks create   --name pgmabv99 --resource-group pgmabv99 \
+# az aks create   --name pgmabv88 --resource-group pgmabv88 \
 --ssh-key-value /home/azureuser/.ssh/authorized_keys \
 --node-count 1
 
-az aks create   --name pgmabv99 --resource-group pgmabv99  --ssh-key-value /home/azureuser/.ssh/authorized_keys  --node-count 1
 # 2. copy credential to local file
-# az aks get-credentials --resource-group pgmabv99 --name pgmabv99
+# az aks get-credentials --resource-group pgmabv88 --name pgmabv88
 
-# 3. add role to allow aks cluster to have external ip
+
+# 3. create external ip 
+# ip to be added to yaml
+# az network public-ip create \
+    --resource-group pgmabv88 \
+    --name aks-ip \
+    --sku Standard \
+    --allocation-method static
+
+# 4. add role to allow aks cluster to have external ip
+## assignee taken from clientid of aks cluster
+## subsription and aks resource group 
 # az role assignment create \
-    --assignee 635d4eec-ce76-48d8-9cc9-082ceda78269 \
+    --assignee 7babc88d-e262-43ac-934d-59197e1bb7bd \
     --role "Network Contributor" \
-    --scope /subscriptions/4b43675f-1b10-4d6a-8e6b-acfa756ef732/resourceGroups/MC_pgmabv99_pgmabv99_westus
-
-# 4. create external ip 
-#az network etc
-
-
+    --scope /subscriptions/cbff8ae6-7d79-4b2d-bf97-d9fc382bc181/resourceGroups/pgmabv88
 
 #utils 
 # az aks stop --name pgmabv99 --resource-group pgmabv99
 # =========show service principal
-#az aks show --name pgmabv99 --resource-group pgmabv99 --query servicePrincipalProfile.clientId -o tsv
+#az aks show --name pgmabv88 --resource-group pgmabv88 --query servicePrincipalProfile.clientId -o tsv
 
